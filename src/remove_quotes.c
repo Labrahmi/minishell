1a85b776d30bc7a92ce821bb8b6351068b25cdd6
youssef labrahmi <ylabrahm@c2r6p8.1337.ma>
2023-05-22 23:58:28 +0100

    22-05/2

--- src/remove_quotes.c
+++ src/remove_quotes.c
@@ -6,7 +6,7 @@
 /*   By: ylabrahm <ylabrahm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 15:22:46 by ylabrahm          #+#    #+#             */
-/*   Updated: 2023/05/22 06:09:10 by ylabrahm         ###   ########.fr       */
+/*   Updated: 2023/05/22 23:19:22 by ylabrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char *get_index(char *string)
 {
 	int	i = 0;
 
-	while (string[i] && string[i] != '\"' && string[i] != ' ')
+	while (string[i] && string[i] != '\"' && string[i] != '\'' && string[i] != ' ')
 		i++;
 	return (ft_substr(string, 0, i));
 }
@@ -47,41 +47,47 @@ void	four_free(char **token, char **suffix, char **env_index, char **env_value)
 		free(*env_value);
 }
 
-/*
-	minishell-1.0> echo "'$HOME'"
-	[1] : echo
-	[2] : "'$HOME'"
-
-
-	=========
-
-	echo "'$HOME'"
-	'/Users/ylabrahm'
-*/
-
-void	expand_loop(char **token, int in_single_quote, int i, t_env *head_env)
+char	*get_new_token(char **token, char *new, t_env *head_env, int i)
 {
 	char	*env_index;
 	char	*env_value;
-	char	*suffix;
-	char	*new;
 	int		len_to;
+	char	*suffix;
+
+	env_index = get_index(&(*token)[i + 1]);
+	env_value = get_value(env_index, &head_env);
+	new = ft_substr((*token), 0, i);
+	new = ft_strjoin(new, env_value);
+	len_to = ft_strlen(&(*token)[i + 1]) - ft_strlen(env_index);
+	suffix = ft_substr(&(*token)[i + 1], ft_strlen(env_index), len_to);
+	new = ft_strjoin(new, suffix);
+	four_free(token, &suffix, &env_index, &env_value);
+	return (new);
+}
+
+void	expand_loop(char **token, int in_single_quote, int in_double_quote, int i, t_env *head_env)
+{
+
+	char	*new;
 
 	new = NULL;
 	while ((*token)[++i])
 	{
-		if ((*token)[i] == '\'')
+
+		if (((*token)[i] == '\'') && (!in_double_quote))
 			in_single_quote = (!in_single_quote);
+		if (((*token)[i] == '\"') && (!in_single_quote))
+			in_double_quote = (!in_double_quote);
 		if ((*token)[i] == '$' && (!in_single_quote))
 		{
-			env_index = get_index(&(*token)[i + 1]);
-			env_value = get_value(env_index, &head_env);
-			new = ft_substr((*token), 0, i);
-			new = ft_strjoin(new, env_value);
-			len_to = ft_strlen(&(*token)[i + 1]) - ft_strlen(env_index);
-			suffix = ft_substr(&(*token)[i + 1], ft_strlen(env_index), len_to);
-			new = ft_strjoin(new, suffix);
-			four_free(token, &suffix, &env_index, &env_value);
+			new = get_new_token(token, new, head_env, i);
+			// env_index = get_index(&(*token)[i + 1]);
+			// env_value = get_value(env_index, &head_env);
+			// new = ft_substr((*token), 0, i);
+			// new = ft_strjoin(new, env_value);
+			// len_to = ft_strlen(&(*token)[i + 1]) - ft_strlen(env_index);
+			// suffix = ft_substr(&(*token)[i + 1], ft_strlen(env_index), len_to);
+			// new = ft_strjoin(new, suffix);
 			(*token) = ft_strdup(new);
 			free(new);
 		}
@@ -91,11 +97,13 @@ void	expand_loop(char **token, int in_single_quote, int i, t_env *head_env)
 char	*expand_variable(char *token, t_env *head_env)
 {
 	int in_single_quote;
+	int in_double_quote;
 	int	i;
 
 	i = -1;
 	in_single_quote = 0;
-	expand_loop(&token, in_single_quote, i, head_env);
+	in_double_quote = 0;
+	expand_loop(&token, in_single_quote, in_double_quote, i, head_env);
 	return (token);
 }
 
